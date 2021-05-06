5c9187cd041d05f1ab125e90d21b7431a8dbfe56
Jady Hom <hyjady@gmail.com>
2021-05-06 14:27:46 -0400

    setup app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./styles/App.css";
+import Router from "../src/components/Router";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router />
     </div>
   );
 }
