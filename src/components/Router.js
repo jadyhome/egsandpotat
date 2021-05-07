8aa110e89d3b9e854e34ecb10fe2df3077d457aa
Jady Hom <hyjady@gmail.com>
2021-05-07 17:46:41 -0400

    2nd initial setup

---
+++ src/components/Router.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+
+const Router = () => {
+  const [pageLoading, setPageLoading] = useState(true);
+
+  useEffect(() => {
+    setPageLoading(false);
+  }, []);
+
+  return (
+    <main>
+      {pageLoading ? (
+        <h1 className="pageloading">PAGE IS LOADING...</h1>
+      ) : (
+        <Switch>
+          <Route exact path="/" component={() => <HomePage />} />
+          {/* <Route path="/eggs" component={() => <EggsPage />} /> */}
+        </Switch>
+      )}
+    </main>
+  );
+};
+
+export default Router;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <section className="eg">eggs</section>
+      <section className="pota">potatoes</section>
+    </div>
+  );
+};
+
+export default HomePage;
