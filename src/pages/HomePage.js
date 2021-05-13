3c7c8aeeedb604285861830052304ee6be84c280
Jady Hom <hyjady@gmail.com>
2021-05-13 17:00:19 -0400

    setup select dropdown

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,8 +3,26 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="homepage">
-      <section className="eg">eggs</section>
-      <section className="pota">potatoes</section>
+      <section className="eg">
+        eggs
+        <div className="dropdown">
+          <select className="">
+            <option value="">🥚🥚🥚</option>
+            <option value="linkhere">option#1</option>
+            <option value="linkhere">option#2</option>
+          </select>
+        </div>
+      </section>
+      <section className="pota">
+        potatoes
+        <div className="dropdown">
+          <select className="">
+            <option value="">🥔🥔🥔</option>
+            <option value="linkhere">option#1</option>
+            <option value="linkhere">option#2</option>
+          </select>
+        </div>
+      </section>
     </div>
   );
 };
