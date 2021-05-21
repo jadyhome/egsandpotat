438ddd3da9373f2d017cfa54dd995b136b427147
Jady Hom <hyjady@gmail.com>
2021-05-21 17:27:20 -0400

    setup landingpage

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="landingpage">
+      <div>IMG HERE</div>
+    </div>
+  );
+};
+
+export default LandingPage;
