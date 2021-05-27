eba14d66f1fcd0f715384ae0967a5267bc2f698c
Jady Hom <hyjady@gmail.com>
2021-05-27 18:33:27 -0400

    setup # 2 for landingpage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Main.css";
 
 const LandingPage = () => {
   return (
     <div className="landingpage">
-      <div>IMG HERE</div>
+      <div className="oven">
+        {
+          <Link to="/home" className="to-homepg">
+            IMG HERE
+          </Link>
+        }
+      </div>
     </div>
   );
 };
