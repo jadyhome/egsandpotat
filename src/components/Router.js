50fb0e194234e7bf8c4b951d67d46282d2261a01
Jady Hom <hyjady@gmail.com>
2021-05-27 18:33:11 -0400

    added two routes for eggrecipe and potatorecipe

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import HomePage from "../pages/HomePage";
+import EggPage from "../pages/EggPage";
+import PotatoPage from "../pages/PotatoPage";
 
 const Router = () => {
   const [pageLoading, setPageLoading] = useState(true);
@@ -18,6 +20,8 @@ const Router = () => {
         <Switch>
           <Route exact path="/" component={() => <LandingPage />} />
           <Route path="/home" component={() => <HomePage />} />
+          <Route path="/eggrecipe" component={() => <EggPage />} />
+          <Route path="/potatorecipe" component={() => <PotatoPage />} />
         </Switch>
       )}
     </main>
