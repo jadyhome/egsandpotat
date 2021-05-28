505b85d6b193a7af5a65fff5419fca58a3c54dcb
Jady Hom <hyjady@gmail.com>
2021-05-28 16:59:38 -0400

    setup frame for recipe icons & linked to recipe page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,11 +6,102 @@ const HomePage = () => {
     <div className="homepage">
       <section className="eg">
         eggs
-        <div className="icons">recipe icons here</div>
+        <div className="recipe-icons">
+          <ul className="icons">
+            <li>
+              {
+                <Link to="/eggrecipe" className="to-egg">
+                  icon
+                </Link>
+              }
+            </li>
+            <li>
+              {
+                <Link to="/eggrecipe" className="to-egg">
+                  icon
+                </Link>
+              }
+            </li>
+            <li>
+              {
+                <Link to="/eggrecipe" className="to-egg">
+                  icon
+                </Link>
+              }
+            </li>
+            <li>
+              {
+                <Link to="/eggrecipe" className="to-egg">
+                  icon
+                </Link>
+              }
+            </li>
+            <li>
+              {
+                <Link to="/eggrecipe" className="to-egg">
+                  icon
+                </Link>
+              }
+            </li>
+            <li>
+              {
+                <Link to="/eggrecipe" className="to-egg">
+                  icon
+                </Link>
+              }
+            </li>
+          </ul>
+        </div>
       </section>
+
       <section className="pota">
         potatoes
-        <div className="icons">recipe icons here</div>
+        <div className="recipe-icons">
+          <ul className="icons">
+            <li>
+              {
+                <Link to="/potatorecipe" className="to-potato">
+                  icon
+                </Link>
+              }
+            </li>
+            <li>
+              {
+                <Link to="/potatorecipe" className="to-potato">
+                  icon
+                </Link>
+              }
+            </li>
+            <li>
+              {
+                <Link to="/potatorecipe" className="to-potato">
+                  icon
+                </Link>
+              }
+            </li>
+            <li>
+              {
+                <Link to="/potatorecipe" className="to-potato">
+                  icon
+                </Link>
+              }
+            </li>
+            <li>
+              {
+                <Link to="/potatorecipe" className="to-potato">
+                  icon
+                </Link>
+              }
+            </li>
+            <li>
+              {
+                <Link to="/potatorecipe" className="to-potato">
+                  icon
+                </Link>
+              }
+            </li>
+          </ul>
+        </div>
       </section>
     </div>
   );
