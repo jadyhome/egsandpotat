ae2f128e3c93ca9257c284032445eb76069d45b4
Jady Hom <hyjady@gmail.com>
2021-12-14 18:10:47 -0500

    created data file for recipes -- egg recipes

---
+++ src/eggpg-data.js
@@ -0,0 +1,76 @@
+import { Link } from "react-router-dom";
+
+export const EggPageData = [
+  {
+    image: (
+      <Link to="/eggrecipe" className="to-egg">
+        <img
+          src="https://image.flaticon.com/icons/png/512/119/119500.png"
+          alt="egg illustration1"
+          className="egg-img"
+        />
+      </Link>
+    ),
+    recipe_name: "egg recipe 1",
+  },
+  {
+    image: (
+      <Link to="/eggrecipe" className="to-egg">
+        <img
+          src="https://image.flaticon.com/icons/png/512/119/119500.png"
+          alt="egg illustration2"
+          className="egg-img"
+        />
+      </Link>
+    ),
+    recipe_name: "egg recipe 2",
+  },
+  {
+    image: (
+      <Link to="/eggrecipe" className="to-egg">
+        <img
+          src="https://image.flaticon.com/icons/png/512/119/119500.png"
+          alt="egg illustration3"
+          className="egg-img"
+        />
+      </Link>
+    ),
+    recipe_name: "egg recipe 3",
+  },
+  {
+    image: (
+      <Link to="/eggrecipe" className="to-egg">
+        <img
+          src="https://image.flaticon.com/icons/png/512/119/119500.png"
+          alt="egg illustration4"
+          className="egg-img"
+        />
+      </Link>
+    ),
+    recipe_name: "egg recipe 4",
+  },
+  {
+    image: (
+      <Link to="/eggrecipe" className="to-egg">
+        <img
+          src="https://image.flaticon.com/icons/png/512/119/119500.png"
+          alt="egg illustration5"
+          className="egg-img"
+        />
+      </Link>
+    ),
+    recipe_name: "egg recipe 5",
+  },
+  {
+    image: (
+      <Link to="/eggrecipe" className="to-egg">
+        <img
+          src="https://image.flaticon.com/icons/png/512/119/119500.png"
+          alt="egg illustration6"
+          className="egg-img"
+        />
+      </Link>
+    ),
+    recipe_name: "egg recipe 6",
+  },
+];
