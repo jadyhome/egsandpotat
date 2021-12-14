28a1637b36cd57478199bdd4e14c55f99231aec3
Jady Hom <hyjady@gmail.com>
2021-12-14 18:11:42 -0500

    organized and imported data file into eggpage

--- src/pages/EggPage.js
+++ src/pages/EggPage.js
@@ -1,64 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { EggPageData } from "../eggpg-data";
 
 const EggPage = () => {
+  const [eggRecipes, setEggRecipes] = useState([]);
+
+  useEffect(() => {
+    fetchEggRecipeData();
+  }, []);
+
+  const fetchEggRecipeData = () => {
+    setEggRecipes(EggPageData);
+  };
+
   return (
     <div className="eggpg">
       <section className="eg">
         eggs
-        <div className="recipe-icons">
-          <ul className="icons">
-            <li>
-              {
-                <Link to="/eggrecipe" className="to-egg">
-                  icon
-                </Link>
-              }
-            </li>
-            <li>
-              {
-                <Link to="/eggrecipe" className="to-egg">
-                  icon
-                </Link>
-              }
-            </li>
-            <li>
-              {
-                <Link to="/eggrecipe" className="to-egg">
-                  icon
-                </Link>
-              }
-            </li>
-            <li>
-              {
-                <Link to="/eggrecipe" className="to-egg">
-                  icon
-                </Link>
-              }
-            </li>
-            <li>
-              {
-                <Link to="/eggrecipe" className="to-egg">
-                  icon
-                </Link>
-              }
-            </li>
-            <li>
-              {
-                <Link to="/eggrecipe" className="to-egg">
-                  icon
-                </Link>
-              }
-            </li>
-          </ul>
+        <div className="recipe-container">
+          {eggRecipes.map((egg, index) => {
+            return (
+              <div key={index} className="recipe-icons">
+                <div className="recipe-img">{egg.image}</div>
+                <p className="recipe-name">{egg.recipe_name}</p>
+              </div>
+            );
+          })}
         </div>
-      <div className="backtoabout">
-        {
-          <Link to="/about" className="to-aboutpg">
-            back to about page
-          </Link>
-        }
-      </div>
+        <div className="backtoabout">
+          {
+            <Link to="/about" className="to-aboutpg">
+              back to about page
+            </Link>
+          }
+        </div>
       </section>
     </div>
   );
