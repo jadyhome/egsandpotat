6783888784e668251fbfd010159d1e704c61c7cd
Jady Hom <hyjady@gmail.com>
2021-12-14 18:11:20 -0500

    created data file for image and name -- potato recipes

---
+++ src/potatopg-data.js
@@ -0,0 +1,76 @@
+import { Link } from "react-router-dom";
+
+export const PotatoPageData = [
+  {
+    image: (
+      <Link to="/potatorecipe" className="to-potato">
+        <img
+          src="https://image.flaticon.com/icons/png/512/605/605251.png"
+          alt="potato illustration6"
+          className="potato-img"
+        />
+      </Link>
+    ),
+    recipe_name: "potato recipe 1",
+  },
+  {
+    image: (
+      <Link to="/potatorecipe" className="to-potato">
+        <img
+          src="https://image.flaticon.com/icons/png/512/605/605251.png"
+          alt="potato illustration6"
+          className="potato-img"
+        />
+      </Link>
+    ),
+    recipe_name: "potato recipe 2",
+  },
+  {
+    image: (
+      <Link to="/potatorecipe" className="to-potato">
+        <img
+          src="https://image.flaticon.com/icons/png/512/605/605251.png"
+          alt="potato illustration6"
+          className="potato-img"
+        />
+      </Link>
+    ),
+    recipe_name: "potato recipe 3",
+  },
+  {
+    image: (
+      <Link to="/potatorecipe" className="to-potato">
+        <img
+          src="https://image.flaticon.com/icons/png/512/605/605251.png"
+          alt="potato illustration6"
+          className="potato-img"
+        />
+      </Link>
+    ),
+    recipe_name: "potato recipe 4",
+  },
+  {
+    image: (
+      <Link to="/potatorecipe" className="to-potato">
+        <img
+          src="https://image.flaticon.com/icons/png/512/605/605251.png"
+          alt="potato illustration6"
+          className="potato-img"
+        />
+      </Link>
+    ),
+    recipe_name: "potato recipe 5",
+  },
+  {
+    image: (
+      <Link to="/potatorecipe" className="to-potato">
+        <img
+          src="https://image.flaticon.com/icons/png/512/605/605251.png"
+          alt="potato illustration6"
+          className="potato-img"
+        />
+      </Link>
+    ),
+    recipe_name: "potato recipe 6",
+  },
+];
